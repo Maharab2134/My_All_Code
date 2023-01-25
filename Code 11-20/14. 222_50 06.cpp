1d5294df8eb1a7ffb03798fede69530bbf4fc0e8
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-01-26 03:26:34 +0600

    Add files via upload

---
+++ Code 11-20/14. 222_50 06.cpp
@@ -0,0 +1,26 @@
+//poblem Number: 791A
+//Povlem Name: Bear and Big Brother
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,count=0;
+    cin>>a>>b;
+    if(a>b)
+    {
+        cout<<0;
+    }
+    else
+    {
+        while(a<=b)
+        {
+            a=3*a;
+            b=2*b;
+            count++;
+        }
+        cout<<count;
+    }
+    return 0;
+}
---
+++ Code 11-20/15. 222_50 06.cpp
@@ -0,0 +1,24 @@
+//poblem Number: 546A
+//Povlem Name: Soldier and Bananas
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int k,n,w,sum=0;
+    cin>>k>>n>>w;
+    for(int i=1;i<=w;i++)
+    {
+        sum+=i*k;
+    }
+    if(sum>n)
+    {
+        cout<<sum-n;
+    }
+    else
+    {
+        cout<<0;
+    }
+    return 0;
+}
