eb2289a9d6bda96af790e4be98033dbe7006eb21
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-01-27 18:30:31 +0600

    Add files via upload

---
+++ Code 21-30/22.222_50 06.cpp
@@ -0,0 +1,25 @@
+//poblem Number: 41A
+//Povlem Name: Tanslation
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    reverse(t.begin(), t.end());
+    if (s == t)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
