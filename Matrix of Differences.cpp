637788fad10c8688a9568633c3a6f2d1b6a11ea2
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-01-29 22:29:08 +0600

    Add files via upload

---
+++ Matrix of Differences.cpp
@@ -0,0 +1,81 @@
+//poblem Number: 1783B
+//Povlem Name: Matrix of Differences
+
+#include<bits/stdc++.h>
+using  namespace  std;
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+#define faster  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+#define sq(x)   (x)*(x)
+#define PI      acos(-1.0)
+#define all(x) x.begin(),x.end()
+#define nl      '\n'
+#define mod 1000000007
+typedef long long int ll;
+typedef unsigned long long int  llu;
+ 
+// PRIME NUMBER TEST//
+bool prime(int n)
+{
+    if(n<2)return false;
+    if(n<=3)return true;
+    if(n%2==0)return false;
+    for(int i=3; i*i<=n; i+=2)
+    {
+        if(n%i==0)return false;
+    }
+    return true;
+}
+ 
+// Graph Grid //
+int dx[]= {-1,0,1,0};
+int dy[]= {0,1,0,-1};
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int x=1,y=n*n;
+        vector<int>v;
+        for(int i=1; i<=n*n; i++)
+        {
+            if(i%2==1)
+            {
+                v.push_back(x);
+                x++;
+            }
+            else
+            {
+                v.push_back(y);
+                y--;
+            }
+        }
+        x=0;
+        for(int i=1; i<=n; i++)
+        {
+            if(i%2==1)
+            {
+                for(int j=x; j<x+n; j++)cout<<v[j]<<" ";
+                cout<<endl;
+                x+=n;
+ 
+            }
+            else
+            {
+                for(int j=(n+x)-1; j>=x; j--)cout<<v[j]<<" ";
+                cout<<endl;
+                x+=n;
+            }
+        }
+    }
+}
