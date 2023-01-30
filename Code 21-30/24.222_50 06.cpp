9080e4b3b46ebe4140127e3233f4a7126a3b9781
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-01-30 23:52:21 +0600

    Add files via upload

---
+++ Code 21-30/24.222_50 06.cpp
@@ -0,0 +1,23 @@
+//poblem Number: 677A
+//Povlem Name:  Vanya and Fence
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll n,m,i,j,h;
+    while(cin>>n>>h){
+        ll a[n+1],stnd=0,bnd=0;
+        for(i=0;i<n;i++){
+            cin>>a[i];
+            if(a[i]>h)
+               bnd++;
+            else
+                stnd++;
+
+        }
+        cout<<2*bnd+stnd<<endl;
+    }
+    return 0;
+}
