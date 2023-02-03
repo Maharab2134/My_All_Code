f751f581ec56516faea05b9ec4468c0d63e9971a
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-02-03 14:30:30 +0600

    Add files via upload

---
+++ Are they Correct .cpp
@@ -0,0 +1,29 @@
+//poblem Number:Contest 02
+//Povlem Name: Are they Correct ??
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int A=0;
+        if(s[0]=='Y' || s[0]=='y'){
+            A++;
+        }
+        if(s[1]=='e' || s[1]=='E'){
+            A++;
+        }
+        if(s[2]=='s' || s[2]=='S'){
+            A++;
+        }
+        if(A==3){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
