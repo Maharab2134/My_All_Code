5cb7074075f5b1b306e0ffb923d11256b5548e84
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-02-03 14:32:14 +0600

    Add files via upload

---
+++ OddSet.cpp
@@ -0,0 +1,20 @@
+//poblem Number: 1542A
+//Povlem Name: OddSet
+
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        long cnt(0);
+        for(long p = 0; p < 2 * n; p++){
+            long x; scanf("%ld", &x);
+            cnt += (x % 2);
+        }
+
+        puts(cnt == n ? "Yes" : "No");
+    }
+
+}
