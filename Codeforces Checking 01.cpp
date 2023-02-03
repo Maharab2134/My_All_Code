90b4565e2d0ca7a798cbf5c914252047386b7fe1
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-02-03 22:56:50 +0600

    Add files via upload

---
+++ Codeforces Checking 01.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for (int j = 0; j < t; j++) {
+        char x;
+        cin>>x;
+        if(x=='c'||x=='o'|| x=='d'|| x=="e" || x=='f' || x=='r' || x=='s'){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+    
+}
